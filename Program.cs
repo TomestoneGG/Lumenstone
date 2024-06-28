e61450c5de6ef4d800b5ac9e30b26b7e72f4faad
TomestoneGG <116524220+TomestoneGG@users.noreply.github.com>
2024-06-27 21:29:11 -0500

    Land 7.0 files
    Land 7.0 files

--- Program.cs
+++ Program.cs
@@ -50,8 +50,8 @@ class Program
         // Call ExtractSheetForAllLanguages for each type
         foreach (var type in types)
         {
-            if (type.Name == "Achievement"  || type.Name == "Quest")
-                continue;
+            //if (type.Name == "Achievement"  || type.Name == "Quest")
+            //    continue;
 
             Console.WriteLine(type.Name);
             
@@ -60,8 +60,8 @@ class Program
         }
 
         // Use our own copy of Achievement to work around the bug with Data being null.
-        ExtractSheetForAllLanguages<Lumenstone.Achievement>(patch, luminaEN, luminaDE, luminaFR, luminaJA, options);
-        ExtractSheetForAllLanguages<Lumenstone.Quest>(patch, luminaEN, luminaDE, luminaFR, luminaJA, options);
+        //ExtractSheetForAllLanguages<Lumenstone.Achievement>(patch, luminaEN, luminaDE, luminaFR, luminaJA, options);
+        //ExtractSheetForAllLanguages<Lumenstone.Quest>(patch, luminaEN, luminaDE, luminaFR, luminaJA, options);
 
         ExtractIcons(1, 156812, luminaEN, full);
         ExtractMaps(luminaEN, full);
